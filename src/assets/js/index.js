ab6de2102eb37afda49aef3f30de0f88f4567128
Liv <liv.heller@techtonic.com>
2021-09-13 15:55:04 -0600

    Adjusted margins for navbar dropdown. Aligned all content center. Built out html

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -4,18 +4,19 @@ function myFunction(x) {
 
     let links = document.getElementsByClassName("nav__item--link");
     let nav = document.getElementsByClassName("nav");
-    let img = document.getElementsByClassName("heroImg");
-    
-    for (let i=0; i < links.length; i++) {
-
-        if (links[i].style.display === "block") {
-            links[i].style.display = "none";
-            nav[0].style.height = "8rem";
-            img[0].style.margin = "4rem 0";
-        } else {
-            links[i].style.display = "block";
-            nav[0].style.height = "30rem";
-            img[0].style.margin = "25rem 0 0 0";
+    // let img = document.getElementsByClassName("heroImg");
+        
+        for (let i=0; i < links.length; i++) {
+            
+            if (links[i].style.display === "block") {
+                links[i].style.display = "none";
+                nav[0].style.height = "8rem";
+                // img[0].style.margin = "4rem 0";
+            } else {
+                links[i].style.display = "block";
+                nav[0].style.height = "30rem";
+                // img[0].style.margin = "25rem 0 0 0";
+            }
         }
-    }
+    
 }
