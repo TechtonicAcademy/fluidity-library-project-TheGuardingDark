b3d1d0575efda3be1810c6d21b06268022105e08
Liv <liv.heller@techtonic.com>
2021-09-18 23:32:03 -0600

    Redid mobile landing and breakpoints

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -3,19 +3,18 @@ function myFunction(x) {
     x.classList.toggle("change");
 
     let links = document.getElementsByClassName("nav__item--link");
-    let nav = document.getElementsByClassName("nav");
-    let img = document.getElementsByClassName("heroImg");
+    let nav = document.getElementsByClassName("nav__container");
     
     for (let i=0; i < links.length; i++) {
 
-        if (links[i].style.display === "block") {
-            links[i].style.display = "none";
-            nav[0].style.height = "8rem";
-            img[0].style.margin = "4rem 0";
-        } else {
+        if (links[i].style.display == "none") {
             links[i].style.display = "block";
-            nav[0].style.height = "30rem";
-            img[0].style.margin = "25rem 0 0 0";
+            nav[0].style.height = "31rem";
+            
+        } else {
+            links[i].style.display === "none";
+            // nav[0].style.height = "30rem";
+            // img[0].style.margin = "25rem 0 0 0";
         }
     }
 }
