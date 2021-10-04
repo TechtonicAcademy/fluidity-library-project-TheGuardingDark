b53b3a87fc245d263da6f62511349f23070d5992
Liv <liv.heller@techtonic.com>
2021-10-04 13:24:38 -0600

    Built out bookshelf view for react, styled and added breakpoints

--- src/pages/Bookshelf.jsx
+++ src/pages/Bookshelf.jsx
@@ -1,12 +1,18 @@
-import { Link } from 'react-router-dom';
+import BookCard from '../components/BookCard';
+import MobileSearch from '../components/MobileSearch';
 
 const Bookshelf = () => {
   return (
-    <div className="bookshelf">
-      <h1>Bookshelf</h1>
-      <Link to="/details">
-        <button type="button">Book Details</button>
-      </Link>
+    <div className="bookshelf grid">
+      <MobileSearch />
+      <h1 className="bookshelf__header">Release the Kraken of Knowledge!</h1>
+      {/* will replace with dynamic rendering once db is up and running */}
+      <BookCard />
+      <BookCard />
+      <BookCard />
+      <BookCard />
+      <BookCard />
+      <BookCard />
     </div>
   );
 };
