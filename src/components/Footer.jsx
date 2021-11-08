fdad3958053d60377c73703ab05bcb54929072da
Liv <liv.heller@techtonic.com>
2021-11-08 14:13:19 -0700

    trying to figure out why files keep getting duplicated when pushing to repo

--- src/components/Footer.jsx
+++
@@ -1,5 +0,0 @@
-const Footer = () => {
-  return <footer className="footer">Copyright 2021</footer>;
-};
-
-export default Footer;
--- src/pages/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-const NotFound = () => {
-  return (
-    <container className="notFound">
-      <h2>404: Page Not Found</h2>
-    </container>
-  );
-};
-
-export default NotFound;
